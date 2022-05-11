33215af4f88d676cfc0694cadf0785a3afb97a90
DeexithParand2k2 <deexith2002@gmail.com>
2022-05-12 02:12:06 +0530

    second commit

---
+++ script/index.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if(document.documentElement.scrollTop <= 40) {
+    document.getElementById("logo").style.width = "10%";
+    document.getElementById("navbar").style.opacity = "1";
+    document.getElementById("navbar").style.boxShadow = "none";
+  } 
+  else if(document.documentElement.scrollTop > 40){
+    document.getElementById("logo").style.width = "7%";
+    document.getElementById("navbar").style.opacity = "0.95";
+    document.getElementById("navbar").style.boxShadow = "0px 2px 10px #d4d6c9";
+  }
+}
