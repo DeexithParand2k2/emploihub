2e3a36da46825d09569131857f917134ca29fdd8
DeexithParand2k2 <deexith2002@gmail.com>
2022-05-21 21:35:26 +0530

    menubar ready

--- script/index.js
+++ script/index.js
@@ -1,23 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if(document.documentElement.scrollTop <= 40 && screen.width>900) {
-    document.getElementById("logo").style.width = "12%";
-    document.getElementById("navbar").style.opacity = "1";
-    document.getElementById("navbar").style.boxShadow = "none";
-  }
-  else if(document.documentElement.scrollTop <= 40 && screen.width<=900) {
-    document.getElementById("logo").style.width = "25%";
-    document.getElementById("navbar").style.opacity = "1";
-    document.getElementById("navbar").style.boxShadow = "none";
-  }
-  else if(document.documentElement.scrollTop > 40 && screen.width<=900) {
-    document.getElementById("logo").style.width = "12%";
+  if(document.documentElement.scrollTop <= 40) {
+    document.getElementById("logo").style.width = "80%";
     document.getElementById("navbar").style.opacity = "1";
     document.getElementById("navbar").style.boxShadow = "none";
   } 
   else if(document.documentElement.scrollTop > 40){
-    document.getElementById("logo").style.width = "7%";
+    document.getElementById("logo").style.width = "60%";
     document.getElementById("navbar").style.opacity = "0.95";
     document.getElementById("navbar").style.boxShadow = "0px 2px 10px #d4d6c9";
   }
