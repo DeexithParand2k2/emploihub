f9aa80eeea298f6e3538ef086b8d1a1634272267
DeexithParand2k2 <deexith2002@gmail.com>
2022-05-23 00:03:21 +0530

    navbar and footer responsive done

--- script/index.js
+++ script/index.js
@@ -2,12 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if(document.documentElement.scrollTop <= 40) {
-    document.getElementById("logo").style.width = "80%";
     document.getElementById("navbar").style.opacity = "1";
     document.getElementById("navbar").style.boxShadow = "none";
   } 
   else if(document.documentElement.scrollTop > 40){
-    document.getElementById("logo").style.width = "60%";
     document.getElementById("navbar").style.opacity = "0.95";
     document.getElementById("navbar").style.boxShadow = "0px 2px 10px #d4d6c9";
   }
