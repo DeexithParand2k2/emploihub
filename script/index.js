167cc018eead3b81688e72a961bc2d5dbdc4949c
DeexithParand2k2 <deexith2002@gmail.com>
2022-06-05 21:18:42 +0530

    beautifying page

--- script/index.js
+++ script/index.js
@@ -2,11 +2,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if(document.documentElement.scrollTop <= 40) {
+    document.getElementById("navbar").style.background = "linear-gradient(to left,#777bac,#ebf2ff,#F9FBFF,#FFFFFF)"
     document.getElementById("navbar").style.opacity = "1";
     document.getElementById("navbar").style.boxShadow = "none";
   } 
   else if(document.documentElement.scrollTop > 40){
-    document.getElementById("navbar").style.opacity = "0.95";
+    document.getElementById("navbar").style.background = "white"
+    document.getElementById("navbar").style.opacity = "0.90";
     document.getElementById("navbar").style.boxShadow = "0px 2px 10px #d4d6c9";
   }
 }
